0899cb1726c5d42b18fbde247041ee1ca9e4b92b
JessAngiolini <jlangiolini@gmail.com>
2025-03-19 19:02:57 -0300

    Actualizo app

--- frontend/app-stock-laflorinda/App.js
+++ frontend/app-stock-laflorinda/App.js
@@ -1,8 +1,15 @@
 
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from "./screens/Home.js"
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/Home";
+import AddProductScreen from "./screens/AddProduct"; 
+import GetProductsScreen from "./screens/GetProducts";
+import UpdatePricesScreen from "./screens/UpdateProduct";
+
+import  TransactionScreen  from "./screens/Transaction";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -10,20 +17,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-        name=' HomeScreen'
-        component={HomeScreen}
-        options={{
-          title : "Stock disponible",
-          headerTitleAlign : 'center',
-          headerTintColor : '#000',
-          headerTitleStyle : {
-            fontWeight : 'bold'
-          }
-
-        }}>
-
-        </Stack.Screen>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="GetProducts" component={GetProductsScreen} />
+        <Stack.Screen name="AddProduct" component={AddProductScreen} />
+        <Stack.Screen name="UpdateProduct" component={UpdatePricesScreen} /> 
+        <Stack.Screen name="Transaction" component={TransactionScreen} /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
