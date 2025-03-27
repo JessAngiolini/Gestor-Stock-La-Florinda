06e76aa53d58fcb3b6f2a2f5cb273f12319ce0a9
JessAngiolini <jlangiolini@gmail.com>
2025-03-26 23:25:55 -0300

    actualizo app

--- frontend/app-stock-laflorinda/App.js
+++ frontend/app-stock-laflorinda/App.js
@@ -1,29 +1,36 @@
 
+  import React from "react";
+  import { NavigationContainer } from "@react-navigation/native";
+  import { createNativeStackNavigator } from "@react-navigation/native-stack";
+  import HomeScreen from "./screens/Home";
+  import { Provider as PaperProvider } from "react-native-paper";
+  import AddProductScreen from "./screens/AddProduct"; 
+  import GetProductsScreen from "./screens/GetProducts";
+  import  TransactionScreen  from "./screens/Transaction";
+  import PurchaseScreen from "./screens/PurchaseScreen";
+  import UpdatePricesScreen from "./screens/updateProduct";
+  import SalesHistoryScreen from "./screens/SalesDetail";
 
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/Home";
-import AddProductScreen from "./screens/AddProduct"; 
-import GetProductsScreen from "./screens/GetProducts";
-import UpdatePricesScreen from "./screens/UpdateProduct";
+  const Stack = createNativeStackNavigator();
 
-import  TransactionScreen  from "./screens/Transaction";
-
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="GetProducts" component={GetProductsScreen} />
-        <Stack.Screen name="AddProduct" component={AddProductScreen} />
-        <Stack.Screen name="UpdateProduct" component={UpdatePricesScreen} /> 
-        <Stack.Screen name="Transaction" component={TransactionScreen} /> 
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+  export default function App() {
+    return (
+      <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ 
+          headerShown: false,
+         }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="GetProducts" component={GetProductsScreen} />
+          <Stack.Screen name="AddProduct" component={AddProductScreen} />
+          <Stack.Screen name="updateProduct" component={UpdatePricesScreen} /> 
+          <Stack.Screen name="Transaction" component={TransactionScreen} /> 
+          <Stack.Screen name="PurchaseScreen" component={PurchaseScreen} />
+          <Stack.Screen name="SalesDetail" component={SalesHistoryScreen} options={{ title: "Historial de Ventas" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      </PaperProvider>
+    );
+  }
 
