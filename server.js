c62fa9080bca4c7900ec3004e948f5d26e7cf3da
JessAngiolini <jlangiolini@gmail.com>
2025-03-31 23:21:30 -0300

    nuevos cambios

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import morgan from "morgan";
 import productRoutes from "./src/routes/productRoutes.js";
 import categoriesRoutes from "./src/routes/categoriesRoutes.js";
 import uploadRoutes from "./src/routes/uploadRoutes.js";
+import saleDetailRoutes from './src/routes/saleDetailRoutes.js' 
+
 
 dotenv.config();
 const app = express();
@@ -16,19 +18,7 @@ app.use(morgan('dev'));
 app.use("/api", categoriesRoutes);
 app.use("/api", productRoutes);
 app.use("/api", uploadRoutes);
-
-// Middleware de manejo de errores
-app.use((err, req, res, next) => {
-  console.error(err.stack); // Imprimir el error en la consola para depuración
-
-  // Responder con un mensaje de error genérico
-  res.status(500).json({
-    error: {
-      message: "Algo salió mal. Por favor, inténtalo de nuevo más tarde.",
-      details: err.message, // Opcional: enviar detalles del error si es necesario
-    },
-  });
-});
+app.use("/api", saleDetailRoutes);
 
 
 const PORT = process.env.PORT || 3000;
