81b937fd825bda86a8c075bf83783880d29b9cd2
JessAngiolini <jlangiolini@gmail.com>
2025-03-31 23:19:59 -0300

    cambios en database

--- src/database/db.js
+++ src/database/db.js
@@ -1,31 +1,33 @@
-import sqlite3 from "sqlite3";
+  import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database("stock.db", (err) => {
-  if (err) {
-    console.error("Error al conectar con la base de datos:", err.message);
-  } else {
-    console.log("Conectado a la base de datos SQLite.");
-  }
-});
+  const db = new sqlite3.Database("stock.db", (err) => {
+    if (err) {
+      console.error("Error al conectar con la base de datos:", err.message);
+    } else {
+      console.log("Conectado a la base de datos SQLite.");
+    }
+  });
 
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS categories (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL
-  )`);
-  
+  db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS categories (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT UNIQUE NOT NULL
+    )`);
+    
+    
+    db.run(`CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT UNIQUE NOT NULL,
+      quantity INTEGER NOT NULL DEFAULT 0,
+      price REAL DEFAULT 0.0,
+      category_id INTEGER,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      update_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
+    )`);
 
-  db.run(`CREATE TABLE IF NOT EXISTS products (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL,
-    quantity INTEGER NOT NULL DEFAULT 0,
-    price REAL DEFAULT 0.0,
-    category_id INTEGER,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    update_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
-  )`);
+    
 
-});
+  });
 
-export default db;
+  export default db;
