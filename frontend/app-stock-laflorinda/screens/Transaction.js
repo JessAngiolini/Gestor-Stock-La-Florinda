6c9691abb0ff9ba413babf23b53f19fc25a460c2
JessAngiolini <jlangiolini@gmail.com>
2025-04-02 22:47:23 -0300

    nuevo screen

---
+++ frontend/app-stock-laflorinda/screens/Transaction.js
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+const TransactionScreen = () => {
+  return (
+    <View style={styles.container}>
+      <LottieView 
+        source={require('../assets/PantallaEnProceso.json')} 
+        autoPlay 
+        loop 
+        style={styles.animation}
+      />
+      <Text style={styles.text}>Estamos trabajando en esta sección 🚧</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f9fa',
+  },
+  animation: {
+    width: 250,
+    height: 250,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#555',
+  },
+});
+
+export default TransactionScreen;
+
